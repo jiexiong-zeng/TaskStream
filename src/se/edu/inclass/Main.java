91462f14a994cfa70d12e536e8453fdfb4f0d75e
Jiexiong <jiexiong123@hotmail.com>
2021-09-17 17:13:57 +0800

    add print data with stream

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -39,6 +39,13 @@ public class Main {
         }
     }
 
+    public static void printDataWithStreams(ArrayList<Task> tasks) {
+        System.out.println("Printing data using stream");
+        tasks.stream()
+                .forEach(System.out::println);
+    }
+
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
